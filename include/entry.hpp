837ae424dc34233b95ac514d4f148dc9ee07f263
Georgi Aleksandrov <george.a.aleksandrov@gmail.com>
2026-07-17 11:35:30 +0300

    Add tradeoff analysis for future eviction algorithms

--- include/entry.hpp
+++ include/entry.hpp
@@ -13,7 +13,17 @@ struct Entry
     }
 
     std::optional<std::string> value;
+
+    // TRADEOFF for having metadata of algos here instead of inside their classes:
+    // + less Key dupliation = less RAM
+    // - coupling between Entry and all future algorithms
+    // - not used metadata depending on the algo used
+
+    // Used by LRU eviction policy
     std::list<std::string>::iterator lruIt;
+
+    // LFU
+    // uint32_t frequency = 0;
 };
 
 #endif
